c8db0f993c525bedb220f1647e03f0639615d5c0
kge123 <88997999+kge123@users.noreply.github.com>
2021-11-09 21:50:59 -0500

    Create read api route, working on write

--- db/index.js
+++ db/index.js
@@ -4,4 +4,33 @@ const util = require('util');
 const uuid = require('uuid/v1');
 
 const readAsync = util.promisify(fs.readFile);
+const writeAsync = util.promisify(fs.writeFile);
 
+class Notes{
+ read(){
+     return readAsync('db/db.json', 'utf-8');
+ }
+
+ write(data){
+     return writeAsync('db/db.json', JSON.stringify(data));
+ }
+
+ readNotes(){
+     return this.read().then((notes)=>{
+         let allNotes;
+
+         try{
+            allNotes = [].concat(JSON.parse(notes))
+         }catch(err){
+            allNotes = []
+         }
+
+         return allNotes;
+     });
+ }
+
+ 
+
+}
+
+module.exports = new Notes();
--- routes/backend.js
+++ routes/backend.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
+const db = require("../db");
 
+//create a route that returns all the notes and renders them on the page
+
+router.get("/notes", (req, res) => {
+  db.readNotes()
+    .then((data) => {
+      return res.json(data);
+    })
+    .catch((err) => res.json(err));
+});
 
 module.exports = router;
